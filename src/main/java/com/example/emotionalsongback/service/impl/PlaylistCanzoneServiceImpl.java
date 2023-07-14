a37bba6b1415bb64f44eda1f23dc8d993d66c261
nazem <nazem.navid@gmail.com>
2023-07-14 16:14:38 +0200

    Completamente autorizzazione anche per Playlist-Canzone. Prima di effettuare le operazioni viene controllato che sia dell'utente.

--- src/main/java/com/example/emotionalsongback/service/impl/PlaylistCanzoneServiceImpl.java
+++ src/main/java/com/example/emotionalsongback/service/impl/PlaylistCanzoneServiceImpl.java
@@ -4,10 +4,12 @@ import com.example.emotionalsongback.dto.CanzoneDto;
 import com.example.emotionalsongback.dto.PlaylistDto;
 import com.example.emotionalsongback.entity.Canzone;
 import com.example.emotionalsongback.entity.Playlist;
+import com.example.emotionalsongback.entity.Utente;
 import com.example.emotionalsongback.exception.APIException;
 import com.example.emotionalsongback.exception.ResourceNotFoundException;
 import com.example.emotionalsongback.repository.CanzoneRepository;
 import com.example.emotionalsongback.repository.PlaylistRepository;
+import com.example.emotionalsongback.service.AuthService;
 import com.example.emotionalsongback.service.PlaylistCanzoneService;
 import com.example.emotionalsongback.service.PlaylistService;
 import lombok.AllArgsConstructor;
@@ -16,23 +18,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class PlaylistCanzoneServiceImpl implements PlaylistCanzoneService {
 
-    CanzoneRepository canzoneRepository;
+    private CanzoneRepository canzoneRepository;
 
-    PlaylistRepository playlistRepository;
+    private PlaylistRepository playlistRepository;
+    private AuthService authService;
 
     ModelMapper modelMapper;
 
 
     @Override
     public void addCanzoneToPlaylist(Long playlistId, CanzoneDto canzoneDto){
+
+        Utente utente = authService.getUtenteFromAuthentication();
+        Set<Playlist> playlists = utente.getPlaylists();
+
         Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(
                 () -> new APIException(HttpStatus.NOT_FOUND, "Playlist non trovate con l'id: " + playlistId));
+
+        if(!playlists.contains(playlist)){
+            throw new APIException(HttpStatus.UNAUTHORIZED, "Playlist con l'id: " + playlistId +" non associata con l'utente: " + utente.getUsername());
+        }
+
         Canzone canzone = modelMapper.map(canzoneDto, Canzone.class);
         playlist.getCanzoni().add(canzone);
         playlistRepository.save(playlist);
@@ -42,10 +55,18 @@ public class PlaylistCanzoneServiceImpl implements PlaylistCanzoneService {
 
     @Override
     public void removeCanzoneFromPlaylist(Long playlistId, Long canzoneId){
+
+        Utente utente = authService.getUtenteFromAuthentication();
+        Set<Playlist> playlists = utente.getPlaylists();
+
         Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(
                 () -> new APIException(HttpStatus.NOT_FOUND, "Playlist non trovate con l'id: " + playlistId));
+        if(!playlists.contains(playlist)){
+            throw new APIException(HttpStatus.UNAUTHORIZED, "Playlist con l'id: " + playlistId +" non associata con l'utente: " + utente.getUsername());
+        }
         Canzone canzone = canzoneRepository.findById(canzoneId).orElseThrow(
                 () -> new APIException(HttpStatus.NOT_FOUND, "Canzone non trovata con id: " + canzoneId));
+
 
         if (!playlist.getCanzoni().contains(canzone)) {
             throw new ResourceNotFoundException("Canzone con id: " + canzoneId + " non è associata con la playlist: " + playlistId);
@@ -57,8 +78,16 @@ public class PlaylistCanzoneServiceImpl implements PlaylistCanzoneService {
 
     @Override
     public PlaylistDto getPlaylistWithCanzoni(Long playlistId){
+
+        Utente utente = authService.getUtenteFromAuthentication();
+        Set<Playlist> playlists = utente.getPlaylists();
+
         Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(
                 () -> new APIException(HttpStatus.NOT_FOUND, "Playlist non trovate con l'id: " + playlistId));
+
+        if(!playlists.contains(playlist)){
+            throw new APIException(HttpStatus.UNAUTHORIZED, "Playlist con l'id: " + playlistId +" non associata con l'utente: " + utente.getUsername());
+        }
 
         PlaylistDto playlistDto = modelMapper.map(playlist, PlaylistDto.class);
 
@@ -67,8 +96,16 @@ public class PlaylistCanzoneServiceImpl implements PlaylistCanzoneService {
 
     @Override
     public List<CanzoneDto> getAllCanzoneInPlaylist(Long playlistId){
+
+        Utente utente = authService.getUtenteFromAuthentication();
+        Set<Playlist> playlists = utente.getPlaylists();
+
         Playlist playlist = playlistRepository.findById(playlistId).orElseThrow(
                 () -> new APIException(HttpStatus.NOT_FOUND, "Playlist non trovate con l'id: " + playlistId));
+
+        if(!playlists.contains(playlist)){
+            throw new APIException(HttpStatus.UNAUTHORIZED, "Playlist con l'id: " + playlistId +" non associata con l'utente: " + utente.getUsername());
+        }
 
         return playlist.getCanzoni().stream().map((canzone -> modelMapper.map(canzone, CanzoneDto.class))).toList();
     }
