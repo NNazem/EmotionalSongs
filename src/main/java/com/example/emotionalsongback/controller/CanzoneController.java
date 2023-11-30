4cec74aa14c03abf9334cba09589572c64b19268
nazem <nazem.navid@gmail.com>
2023-11-30 11:05:47 +0100

    Implementanzione paginazione per ricerca titolo e autore.

--- src/main/java/com/example/emotionalsongback/controller/CanzoneController.java
+++ src/main/java/com/example/emotionalsongback/controller/CanzoneController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Controller per la gestione delle richieste relative alle canzoni.
+ * Fornisce endpoint per la ricerca e il recupero di informazioni sulle canzoni.
+ */
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("api/canzone")
@@ -21,37 +25,53 @@ public class CanzoneController {
     private CanzoneService canzoneService;
 
     /**
-     * Gestisce la richiesta GET per ricercare canzoni tramite titolo.
+     * Ottiene una lista di canzoni filtrate per titolo.
      *
-     * @param titolo il titolo della canzone da cercare
-     * @return una ResponseEntity che contiene una lista di oggetti CanzoneDto e lo stato HTTP OK
+     * @param titolo Il titolo della canzone da cercare.
+     * @param orderBy Campo per l'ordinamento delle canzoni (default "id").
+     * @param orderDirection Direzione dell'ordinamento, ASC o DESC (default "ASC").
+     * @param page Numero di pagina per la paginazione dei risultati (default "1").
+     * @return ResponseEntity contenente una lista di CanzoneDto e lo stato HTTP OK.
      */
     @GetMapping
     public ResponseEntity<CanzoniResponse> getCanzoniByTitolo(@RequestParam("titolo") String titolo,
-                                                               @RequestParam(value = "orderBy", required = false, defaultValue = "id") String orderBy,
-                                                               @RequestParam(value = "orderDirection", required = false, defaultValue = "ASC") String orderDirection,
-                                                               @RequestParam(value = "page", required = true, defaultValue = "1") String page){
+                                                              @RequestParam(value = "orderBy", required = false, defaultValue = "id") String orderBy,
+                                                              @RequestParam(value = "orderDirection", required = false, defaultValue = "ASC") String orderDirection,
+                                                              @RequestParam(value = "page", required = true, defaultValue = "1") String page){
         CanzoniResponse getCanzoni = canzoneService.getCanzoniByTitolo(titolo, orderBy, orderDirection, page);
         return ResponseEntity.ok(getCanzoni);
     }
 
     /**
-     * Gestisce la richiesta GET per ricercare canzoni tramite autore e anno.
+     * Ottiene una lista di canzoni filtrate per autore e anno.
      *
-     * @param anno l'anno della canzone da cercare
-     * @param autore l'autore della canzone da cercare
-     * @return una ResponseEntity che contiene una lista di oggetti CanzoneDto e lo stato HTTP OK
+     * @param anno L'anno della canzone da cercare.
+     * @param autore L'autore della canzone da cercare.
+     * @param orderBy Campo per l'ordinamento delle canzoni (default "id").
+     * @param orderDirection Direzione dell'ordinamento, ASC o DESC (default "ASC").
+     * @param page Numero di pagina per la paginazione dei risultati (default "1").
+     * @return ResponseEntity contenente una lista di CanzoneDto e lo stato HTTP OK.
      */
     @GetMapping("/byAutoreAndAnno")
     public ResponseEntity<CanzoniResponse> getCanzoniByAnnoAndAutore(@RequestParam("anno") String anno,
-                                                                      @RequestParam("autore") String autore,
-                                                                      @RequestParam(value = "orderBy", required = false, defaultValue = "id") String orderBy,
-                                                                      @RequestParam(value = "orderDirection", required = false, defaultValue = "ASC") String orderDirection,
-                                                                      @RequestParam(value = "page", required = true, defaultValue = "1") String page){
+                                                                     @RequestParam("autore") String autore,
+                                                                     @RequestParam(value = "orderBy", required = false, defaultValue = "id") String orderBy,
+                                                                     @RequestParam(value = "orderDirection", required = false, defaultValue = "ASC") String orderDirection,
+                                                                     @RequestParam(value = "page", required = true, defaultValue = "1") String page){
         CanzoniResponse getCanzoni = canzoneService.getCanzoniByAutoreAndAnno(anno,autore, orderBy, orderDirection, page);
         return ResponseEntity.ok(getCanzoni);
     }
 
+    /**
+     * Ottiene una lista di canzoni filtrate per titolo e autore.
+     *
+     * @param titolo Il titolo della canzone da cercare.
+     * @param autore L'autore della canzone da cercare.
+     * @param orderBy Campo per l'ordinamento delle canzoni (default "id").
+     * @param orderDirection Direzione dell'ordinamento, ASC o DESC (default "ASC").
+     * @param page Numero di pagina per la paginazione dei risultati (default "1").
+     * @return ResponseEntity contenente una lista di CanzoneDto e lo stato HTTP OK.
+     */
     @GetMapping("/byTitoloAndAutore")
     public ResponseEntity<CanzoniResponse>  getCanzoniByTitoloAndAutore(@RequestParam("titolo") String titolo,
                                                                         @RequestParam("autore") String autore,
